0275897fa1efdda06848a17424394fe668783b19
vitaliesmentanca <56801290+vitaliesmentanca@users.noreply.github.com>
2021-02-22 17:59:31 -0600

    first commit

---
+++ src/test/java/utilities/ConfigurationReader.java
@@ -0,0 +1,32 @@
+package utilities;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigurationReader {
+    /* In this class we will implement the repeated steps of reading
+    from configuration.properties file*/
+
+    //1.Create the object of properties
+    private  static Properties properties =new Properties();
+    static {
+        //2.Get the path and open the file
+        try {
+            //3- we need to open the file
+            //2- get the path and store in String, or directly pass into fileInputStream obj
+            //String ^path = "configuration.properties";
+            FileInputStream file=new FileInputStream("configuration.properties");//<-- ^path
+            //3.Load the opened file into properties object
+            properties.load(file);
+            //closing the file in JVM Memory
+            file.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    // 4.Use the object to read from the configuration .properties file
+    public  static String getProperty(String keyWord){
+        return  properties.getProperty(keyWord);
+    }
+}
---
+++ src/test/java/utilities/Driver.java
@@ -0,0 +1,58 @@
+package utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class Driver {
+    private Driver() {
+    }
+
+    private static ThreadLocal<WebDriver> driverPool = new ThreadLocal<>();
+
+    public static WebDriver getDriver() {
+
+
+        if (driverPool.get() == null) {//if driver is not existing it will go to switch case
+            synchronized (Driver.class) {
+                String browser = ConfigurationReader.getProperty("browser");
+                //if configuration file not set it will go to default ↓ switch block
+                switch (browser) {
+                    case "chrome":
+                        WebDriverManager.chromedriver().setup();
+                        driverPool.set(new ChromeDriver());
+                        driverPool.get().manage().window().maximize();
+                        driverPool.get().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                        break;
+                    case "firefox":
+                        WebDriverManager.firefoxdriver().setup();
+                        driverPool.set(new FirefoxDriver());
+                        driverPool.get().manage().window().maximize();
+                        driverPool.get().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+                        break;
+                    default://and here will open by default chrome ↓
+                        WebDriverManager.chromedriver().setup();
+                        driverPool.set(new ChromeDriver());
+                        driverPool.get().manage().window().maximize();
+                        driverPool.get().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+                        break;
+                }
+            }
+        }
+        //if driver is not null it will jump here ↓
+        return driverPool.get();
+    }
+
+    public static void closeDriver() {
+        if (driverPool.get() != null) {
+            driverPool.get().quit();
+            driverPool.remove();//to reuse again and go on top↑ to if condition(getDriver)
+            //
+        }
+    }
+}
