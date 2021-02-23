50f325b2fecee6a36ad4cc8389b0c99f641e0b1a
vitaliesmentanca <56801290+vitaliesmentanca@users.noreply.github.com>
2021-02-22 18:13:12 -0600

    added com... package

--- src/test/java/com/cybertek/utilities/BrowserUtils.java
+++ src/test/java/com/cybertek/utilities/BrowserUtils.java
@@ -1,4 +1,4 @@
-package utilities;
+package com.cybertek.utilities;
 
 import org.junit.Assert;
 import org.openqa.selenium.*;
--- src/test/java/com/cybertek/utilities/ConfigurationReader.java
+++ src/test/java/com/cybertek/utilities/ConfigurationReader.java
@@ -1,4 +1,4 @@
-package utilities;
+package com.cybertek.utilities;
 
 import java.io.FileInputStream;
 import java.io.IOException;
